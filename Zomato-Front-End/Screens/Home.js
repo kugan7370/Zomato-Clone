9108285475b45b61af02578f3549f62410bbcd3b
kugan18 <kugapriyanj97@gmail.com>
2023-01-21 13:49:28 +0530

    added tailwin css

---
+++ Zomato-Front-End/Screens/Home.js
@@ -0,0 +1,10 @@
+import { View, Text, SafeAreaView } from 'react-native'
+import React from 'react'
+
+export default function Home() {
+    return (
+        <View>
+            <Text className="text-red-400">Home</Text>
+        </View>
+    )
+}
