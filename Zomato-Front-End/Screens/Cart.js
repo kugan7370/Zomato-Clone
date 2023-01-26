b8bb6dfab1831de95261889fcf433ba90f3e38ff
kugan18 <kugapriyanj97@gmail.com>
2023-01-26 12:35:00 +0530

    added divider

--- Zomato-Front-End/Screens/Cart.js
+++ Zomato-Front-End/Screens/Cart.js
@@ -40,17 +40,19 @@ export default function Cart() {
                     <Text className="font-bold text-white tracking-wider">Discount</Text>
                     <Text className="text-white">$ 5.00</Text>
                 </View>
-                <View className="flex-row justify-between items-center mt-4 ">
+
+                <View className=" h-[1px] w-full bg-slate-500 my-1" />
+                <View className="flex-row justify-between items-center mt-2 ">
                     <Text className="font-bold text-white text-xl tracking-wider">Total</Text>
                     <Text className="text-white text-xl">$ 60.00</Text>
                 </View>
             </View>
 
             {/* button */}
-            <View className='justify-center item-center bg-primary-100 py-2 px-4 mt-8 rounded-xl'>
+            <View className='justify-center item-center bg-primary-100 py-2 px-4 mt-6 rounded-xl'>
                 <Text className="text-lg font-bold text-center text-white">Proceed to pay</Text>
             </View>
 
-        </View>
+        </View >
     )
 }
